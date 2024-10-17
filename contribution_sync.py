3e15dc683b4298846913ec6e3cd52c7968fa5aaa
Kai Meinerz <kmeinerz@thp.uni-koeln.de>
2024-10-17 10:48:19 +0200

    wip

--- contribution_sync.py
+++ contribution_sync.py
@@ -1,14 +1,24 @@
 import git
+import os
 # from src.Importer import *
 
 from src.Content import *
 from src.Importer import *
 from src.Committer import *
 
-repo = git.Repo("C:/Users/Kaime/Desktop/Work/small_codes")
-mock_repo = git.Repo("C:/Users/Kaime/Desktop/Work/MockRepos/small_codes")
 
-importer = Importer([repo], mock_repo)
-importer.set_author('kmeinerz@thp.uni-koeln.de')
+folder_path = '/home/kmeinerz/Desktop/Work/'
+# list_repos = ['rsc', 'qec', 'paper_qec', 'qec_code', 'ft_qec', 'topologicalcodes', 'machinelearning', 'thesis', 'small_codes']
+list_repos = ['small_codes']
 
-importer.import_repository()
+
+for repos in list_repos:
+    print(f'syncing {repos}')
+    repo = git.Repo(f"{folder_path}/{repos}")
+    mock_repo = git.Repo(f"{folder_path}/MockRepos/{repos}")
+
+    importer = Importer([repo], mock_repo)
+    importer.set_author('kmeinerz@thp.uni-koeln.de')
+
+    importer.import_repository()
+    os.system( f"cd {folder_path}MockRepos/{repos}/ \n git push" )
